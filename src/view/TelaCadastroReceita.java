45329bd5e783ee7c9c2af3a55c198ef4f5a09ca8
lucaseduardotestoni <testonilucas015@gmail.com>
2023-11-26 18:02:10 -0300

    Lucas: Subindo alterações

--- src/view/TelaCadastroReceita.java
+++ src/view/TelaCadastroReceita.java
@@ -295,7 +295,7 @@ public class TelaCadastroReceita extends javax.swing.JFrame {
         if(tipoReceita.getSelectedItem() == null || dataReceita.getText().trim().equals("") || valorReceita.getText().trim().equals("")){
             JOptionPane.showConfirmDialog(this.rootPane,"Preencha todos os campos");
         }else{
-            receita.setCategoria((CategoriaReceita) tipoReceita.getSelectedItem());
+            receita.setCategoria(CategoriaReceita.valueOf(tipoReceita.getSelectedItem().toString()));
             receita.setValor(Double.parseDouble(valorReceita.getText()));
             String dataTexto = dataReceita.getText();
             SimpleDateFormat formatoData = new SimpleDateFormat("MM/dd/yyyy");
